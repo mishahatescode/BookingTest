3580cd548c8574aebc1e4d206723b4738ddaccd0
Misha <mishalovesspam@gmail.com>
2024-09-14 17:09:14 +0800

    Fix: API Key Handling Epic

--- pages/api/booking-proxy.ts
+++ pages/api/booking-proxy.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+//import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
