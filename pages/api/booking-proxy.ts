785f4ca2ec09c752b181803738b6714ec7253c87
Misha <mishalovesspam@gmail.com>
2024-09-14 17:37:07 +0800

    Fix: API Key Handling Epic

--- pages/api/booking-proxy.ts
+++ pages/api/booking-proxy.ts
@@ -36,5 +36,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   } else {
     res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
+    console.log('Request method:', req.method);
   }
 }
